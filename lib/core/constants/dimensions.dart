06c90c4190b6f076948b61b2c40fd7b46a3e48c4
DoReMinWoo <kimy5878@naver.com>
2024-12-13 23:21:38 +0900

    add #27 constants SizedBox, EdgeInsets 상수화

---
+++ lib/core/constants/dimensions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Dimensions {
+  static const EdgeInsets all16 = EdgeInsets.symmetric(horizontal: 16);
+
+  static const EdgeInsets horizontal12 = EdgeInsets.symmetric(horizontal: 12);
+  static const EdgeInsets horizontal16 = EdgeInsets.symmetric(horizontal: 16);
+
+  static const EdgeInsets vertical12 = EdgeInsets.symmetric(vertical: 12);
+  static const EdgeInsets vertical16 = EdgeInsets.symmetric(vertical: 16);
+}
