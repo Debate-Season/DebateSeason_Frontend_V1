a4b53a03db8ea52b72c501dfd94d07e2111a74e7
DoReMinWoo <kimy5878@naver.com>
2024-12-13 23:06:21 +0900

    add #27 text style
    largest, headerLarge, body16R, body14R, body12SB, body12R

--- lib/core/constants/text_style.dart
+++ lib/core/constants/text_style.dart
@@ -1,6 +1,22 @@
 import 'package:debateseason_frontend_v1/core/constants/color.dart';
 import 'package:flutter/material.dart';
 
+const TextStyle largest = TextStyle(
+  color: grey10,
+  fontSize: 48,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w700,
+  height: 1,
+);
+
+const TextStyle headerLarge = TextStyle(
+  color: grey10,
+  fontSize: 28,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w700,
+  height: 1.5,
+);
+
 const TextStyle header = TextStyle(
   color: grey10,
   fontSize: 20,
@@ -33,6 +49,14 @@ const TextStyle body16M = TextStyle(
   height: 1.5,
 );
 
+const TextStyle body16R = TextStyle(
+  color: grey10,
+  fontSize: 16,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w400,
+  height: 1.5,
+);
+
 const TextStyle body14Sb = TextStyle(
   color: grey10,
   fontSize: 14,
@@ -49,10 +73,34 @@ const TextStyle body14M = TextStyle(
   height: 1.57142,
 );
 
+const TextStyle body14R = TextStyle(
+  color: grey10,
+  fontSize: 14,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w400,
+  height: 1.57142,
+);
+
+const body12SB = TextStyle(
+  color: grey10,
+  fontSize: 14,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w600,
+  height: 1.5,
+);
+
 const body12M = TextStyle(
   color: grey10,
   fontSize: 14,
   fontFamily: 'Pretendard',
   fontWeight: FontWeight.w500,
+  height: 1.5,
+);
+
+const body12R = TextStyle(
+  color: grey10,
+  fontSize: 14,
+  fontFamily: 'Pretendard',
+  fontWeight: FontWeight.w400,
   height: 1.5,
 );
