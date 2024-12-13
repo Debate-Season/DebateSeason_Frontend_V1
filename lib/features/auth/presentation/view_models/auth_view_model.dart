3e7db3974b8c13435c04f30bd2c8c87c07a1a6b6
DoReMinWoo <kimy5878@naver.com>
2024-12-14 00:18:44 +0900

    feat #27 auth screen ui

--- lib/features/auth/presentation/view_models/auth_view_model.dart
+++ lib/features/auth/presentation/view_models/auth_view_model.dart
@@ -1,33 +1,6 @@
-import 'package:debateseason_frontend_v1/features/auth/data/repositories_impls/remote/auth_repository.dart';
-import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class AuthViewModel extends ChangeNotifier {
-  final AuthRepository _authRepository;
-  bool _isLoading = false;
-  bool _isLoggedIn = false;
-
-  AuthViewModel(this._authRepository);
-
-  bool get isLoggedIn => _isLoggedIn;
-  bool get isLoading => _isLoading;
-
-  Future<bool> login(String username, String password) async {
-    try {
-      _isLoading = true;
-      notifyListeners();
-
-      final success = await _authRepository.login(username, password);
-
-      if (success) {
-        _isLoggedIn = true;
-      } else {
-        _isLoggedIn = false;
-      }
-
-      return success;
-    } finally {
-      _isLoading = false;
-      notifyListeners();
-    }
-  }
+class AuthViewModel extends GetxController {
+  final String kakaoLoginType = 'kakao';
+  final String appleLoginType = 'apple';
 }
