95c0bb415d3754cc0249632da728455008343dab
Anzinius <anzinius@gmail.com>
2024-12-22 02:26:50 +0900

    add #29 constants 코드에 필요한 상수 추가
    - EdgeInsets all10
    - EdgeInsets all16

--- lib/core/constants/dimensions.dart
+++ lib/core/constants/dimensions.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Dimensions {
-  static const EdgeInsets all16 = EdgeInsets.symmetric(horizontal: 16);
+  static const EdgeInsets all8 = EdgeInsets.symmetric(horizontal: 8, vertical: 8);
+  static const EdgeInsets all10 = EdgeInsets.symmetric(horizontal: 10, vertical: 10);
+  static const EdgeInsets all16 = EdgeInsets.symmetric(horizontal: 16, vertical: 16);
 
   static const EdgeInsets padding12x16 =
       EdgeInsets.symmetric(horizontal: 12, vertical: 16);
