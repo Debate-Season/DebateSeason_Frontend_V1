d3700742c4314d7e25444f7482732150a71b4323
Anzinius <anzinius@gmail.com>
2024-12-22 00:49:14 +0900

    add #29 디버그용 코드(log.d) 추가

--- lib/core/web_socket/stomp_service.dart
+++ lib/core/web_socket/stomp_service.dart
@@ -1,5 +1,7 @@
 import 'package:stomp_dart_client/stomp_dart_client.dart';
 
+import '../../utils/logger.dart';
+
 class StompService {
   late StompClient stompClient;
 
@@ -7,18 +9,45 @@ class StompService {
     stompClient = StompClient(
       config: StompConfig.sockJS(
         url: url,
-        onConnect: onConnect,
-        onWebSocketError: onError,
+        // onConnect: onConnect,
+        // onWebSocketError: onError,
+        onConnect: (frame) {
+          log.d("STOMP 연결 성공: ${frame.headers}");
+          onConnect(frame);
+        },
+        onWebSocketError: (error) {
+          log.d("WebSocket 에러: $error");
+          onError(error);
+        },
+        onStompError: (frame) {
+          log.d("STOMP 프로토콜 에러: ${frame.body}");
+        },
+        onDisconnect: (frame) {
+          log.d("STOMP 연결 해제: ${frame.headers}");
+        },
+        onWebSocketDone: () {
+          log.d("WebSocket 연결 종료");
+        },
       ),
     );
     stompClient.activate();
+    log.d("STOMP 클라이언트 활성화...");
   }
 
   void subscribe(String destination, Function(String) onMessage){
-    stompClient.subscribe(
-      destination: destination,
-      callback: (frame) => onMessage(frame.body!),
-    );
+    log.d('* 구독 시도: $destination');
+    try {
+      stompClient.subscribe(
+          destination: destination,
+          //callback: (frame) => onMessage(frame.body!),
+          callback: (frame) {
+            log.d('* 구독 성공: $destination, 받은 메시지: ${frame.body}');
+            onMessage(frame.body ?? "");
+          }
+      );
+    } catch (e){
+      log.d('* 구독 실패: $e');
+    }
   }
 
   void sendMessage(String destination, String message) {
--- lib/features/chat/presentation/view_model/chat_room_view_model.dart
+++ lib/features/chat/presentation/view_model/chat_room_view_model.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:debateseason_frontend_v1/core/web_socket/stomp_service.dart';
 import 'package:debateseason_frontend_v1/features/chat/data/models/request/message_request.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:debateseason_frontend_v1/utils/logger.dart';
 import 'package:get/get.dart';
 
 import '../../data/models/response/message_response.dart';
@@ -17,21 +17,34 @@ class ChatRoomViewModel extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    log.d('1. init');
+
     _stompService.connect(
       dotenv.get("WEB_SOCKET_BASE_URL"),
-      (frame) => log('chat server 연결!'),
-      (error) => log('chat server 에러: $error'),
+      (frame) {
+        log.d('2. chat server 연결!: ${frame.headers}');
+        _stompService.subscribe('/chat.room.1', (msg) {
+          log.d('3. 서버에서 받은 메세지: $msg');
+          if (msg != null) {
+            try {
+              MessageResponse messageResponse = MessageResponse.fromJson(jsonDecode(msg));
+              receivedMessages.add(messageResponse);
+              log.d('4. 구독 연결~');
+            } catch (e){
+              log.d('4. 구독 실패: $e');
+            }
+          }
+        });
+      },
+      (error) => log.d('2. chat server 에러: $error'),
     );
-    _stompService.subscribe('/topic/public', (msg) {
-      MessageResponse messageResponse = MessageResponse.fromJson(jsonDecode(msg));
-      receivedMessages.add(messageResponse);
-    });
   }
 
   void sendMessage(MessageRequest messageRequest) {
     // _stompService.sendMessage('/app/chat.sendMessage', message);
     String message = jsonEncode(messageRequest.toJson());
-    _stompService.sendMessage('/app/chat.sendMessage', message);
+    // _stompService.sendMessage('/app/chat.sendMessage', message);
+    _stompService.sendMessage('/topic/room1', message);
     sentMessages.add(messageRequest);
   }
 
