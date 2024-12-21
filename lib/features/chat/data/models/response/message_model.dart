5f65a63f340c5e851e23c1e29c0472a6b0ccbe6b
Anzinius <anzinius@gmail.com>
2024-12-21 12:46:31 +0900

    mod #29 message model 수정

--- lib/features/chat/data/models/response/message_model.dart
+++ lib/features/chat/data/models/response/message_model.dart
@@ -1,27 +1,16 @@
 class MessageModel {
   final String sender;
   final String content;
-  //final DateTime timestamp;
   final String category;
 
   MessageModel(
-      {required this.sender, required this.category, required this.content});
+      {required this.sender, required this.content, required this.category});
 
   factory MessageModel.fromJson(Map<String, dynamic> json) {
     return MessageModel(
       sender: json['sender'],
-      category: json['category'],
       content: json['content'],
-      //timestamp: DateTime.parse(json['timestamp']),
+      category: json['category'],
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'sender': sender,
-      'category': category,
-      'content': content,
-      //'timestamp': timestamp.toIso8601String(),
-    };
   }
 }
