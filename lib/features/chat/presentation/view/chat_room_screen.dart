ca184f51310e2dcfcaf861d77b1cdfb559511dc0
Anzinius <anzinius@gmail.com>
2024-12-21 14:15:30 +0900

    feat #29 ChatRoomScreen에 GetView 활용
    - 초기 화면 구성

--- lib/features/chat/presentation/view/chat_room_screen.dart
+++ lib/features/chat/presentation/view/chat_room_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../view_model/chat_room_view_model.dart';
 
-class ChatRoomScreen extends StatelessWidget {
-  final TextEditingController _controller = TextEditingController();
-
+class ChatRoomScreen extends GetView<ChatRoomViewModel>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,46 +11,73 @@ class ChatRoomScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          // Expanded(
-          //   child: Consumer<ChatRoomViewModel>(
-          //     builder: (context, viewModel, child) {
-          //       return ListView.builder(
-          //         itemCount: viewModel.messages.length,
-          //         itemBuilder: (context, index) {
-          //           final message = viewModel.messages[index];
-          //           return MessageBubble(message: message);
-          //         },
-          //       );
-          //     },
-          //   ),
-          // ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(labelText: "의견을 입력하세요..."),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () {
-                    if (_controller.text.isNotEmpty) {
-                      // context
-                      //     .read<ChatRoomViewModel>()
-                      //     .sendMessage(_controller.text);
-                      _controller.clear();
-                    }
-                  },
-                ),
-              ],
-            ),
+          Expanded(
+            child: Obx(() => ListView.builder(
+            itemCount: controller.messages.length,
+            itemBuilder: (context, index){
+            return ListTile(title: Text(controller.messages[index]));
+          },)),),
+          TextField(
+            onSubmitted: (value) => controller.sendMessage(value),
+            decoration: InputDecoration(hintText: '의견을 입력하세요...'),
           )
         ],
       ),
     );
-    //throw UnimplementedError();
   }
+
 }
+// class ChatRoomScreen extends StatelessWidget {
+//   final TextEditingController _controller = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("토론방"),
+//       ),
+//       body: Column(
+//         children: [
+//           // Expanded(
+//           //   child: Consumer<ChatRoomViewModel>(
+//           //     builder: (context, viewModel, child) {
+//           //       return ListView.builder(
+//           //         itemCount: viewModel.messages.length,
+//           //         itemBuilder: (context, index) {
+//           //           final message = viewModel.messages[index];
+//           //           return MessageBubble(message: message);
+//           //         },
+//           //       );
+//           //     },
+//           //   ),
+//           // ),
+//           Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Row(
+//               children: [
+//                 Expanded(
+//                   child: TextField(
+//                     controller: _controller,
+//                     decoration: InputDecoration(labelText: "의견을 입력하세요..."),
+//                   ),
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.send),
+//                   onPressed: () {
+//                     if (_controller.text.isNotEmpty) {
+//                       // context
+//                       //     .read<ChatRoomViewModel>()
+//                       //     .sendMessage(_controller.text);
+//                       _controller.clear();
+//                     }
+//                   },
+//                 ),
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//     //throw UnimplementedError();
+//   }
+// }
