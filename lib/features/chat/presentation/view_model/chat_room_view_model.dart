28419e6c992853e8e8b954970823571ed818bc13
Anzinius <anzinius@gmail.com>
2024-12-21 14:12:22 +0900

    feat #29 채팅기능을 위한 stomp 연결 설정

--- lib/features/chat/presentation/view_model/chat_room_view_model.dart
+++ lib/features/chat/presentation/view_model/chat_room_view_model.dart
@@ -1,33 +1,32 @@
+import 'dart:developer';
+
 import 'package:debateseason_frontend_v1/core/web_socket/stomp_service.dart';
-import 'package:debateseason_frontend_v1/features/chat/data/models/response/message_model.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
-class ChatRoomViewModel with ChangeNotifier {
+class ChatRoomViewModel extends GetxController {
   final StompService _stompService = StompService();
-  List<MessageModel> messages = [];
 
-  ChatRoomViewModel() {
-    _stompService.connect('your id', (message) {
-      messages.add(message);
-      notifyListeners();
-    });
+  var messages = <String>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _stompService.connect(
+      dotenv.get("WEB_SOCKET_BASE_URL"),
+      (frame) => log('chat server 연결!'),
+      (error) => log('chat server 에러: $error'),
+    );
+    _stompService.subscribe('/topic/public', (msg) => messages.add(msg));
   }
 
-  void sendMessage(String content) {
-    var message = MessageModel(
-      sender: 'your id',
-      category: 'category',
-      content: content,
-      //timestamp: DateTime.now(),
-    );
-    _stompService.sendMessage(message);
-    messages.add(message);
-    notifyListeners();
+  void sendMessage(String message) {
+    _stompService.sendMessage('/app/chat.sendMessage', message);
   }
 
   @override
-  void dispose() {
+  void onClose(){
     _stompService.disconnect();
-    super.dispose();
+    super.onClose();
   }
 }
