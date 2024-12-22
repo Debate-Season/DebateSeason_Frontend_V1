92767cc8842cbeedf3646eadabfada56cc85f5e9
Anzinius <anzinius@gmail.com>
2024-12-22 22:49:46 +0900

    add #29 메세지 리스트 위젯분리

--- lib/features/chat/presentation/view/chat_room_screen.dart
+++ lib/features/chat/presentation/view/chat_room_screen.dart
@@ -1,11 +1,10 @@
-import 'package:debateseason_frontend_v1/features/chat/data/models/request/message_request.dart';
 import 'package:debateseason_frontend_v1/features/chat/presentation/widgets/chat_input_field.dart';
+import 'package:debateseason_frontend_v1/features/chat/presentation/widgets/chat_message_list.dart';
 import 'package:debateseason_frontend_v1/features/chat/presentation/widgets/chat_room_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../data/models/response/message_response.dart';
+import '../../../../core/constants/color.dart';
 import '../view_model/chat_room_view_model.dart';
-import 'message_bubble.dart';
 
 class ChatRoomScreen extends GetView<ChatRoomViewModel>{
   final ChatRoomViewModel _viewModel = Get.find<ChatRoomViewModel>();
@@ -15,21 +14,12 @@ class ChatRoomScreen extends GetView<ChatRoomViewModel>{
     TextEditingController messageController = TextEditingController();
 
     return Scaffold(
+      backgroundColor: grey80,
       appBar: const ChatRoomAppBar(title: '토론철은 얼마나 대단한가'),
       body: Column(
         children: [
           Expanded(
-            child: Obx(() => ListView.builder(
-              itemCount: _viewModel.receivedMessages.length,
-              itemBuilder: (context, index){
-                MessageResponse messageResponse = _viewModel.receivedMessages[index];
-                MessageRequest messageRequest = MessageRequest(
-                  sender: messageResponse.sender,
-                  content: messageResponse.content,
-                );
-                return MessageBubble(message: messageRequest);
-              },
-            ),),
+            child: ChatMessageList(),
           ),
           ChatInputField(
               controller: messageController,
