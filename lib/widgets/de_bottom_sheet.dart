78a0533133afa3e7f5f61cf031cbeef132238e31
Doreminwoo <kimy5878@naver.com>
2024-12-22 17:06:53 +0900

    mod #31 재사용 바텀시트 unfocus 추가

--- lib/widgets/de_bottom_sheet.dart
+++ lib/widgets/de_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:debateseason_frontend_v1/core/constants/color.dart';
 import 'package:debateseason_frontend_v1/core/constants/dimensions.dart';
 import 'package:debateseason_frontend_v1/core/constants/gaps.dart';
 import 'package:debateseason_frontend_v1/core/constants/text_style.dart';
+import 'package:debateseason_frontend_v1/widgets/de_gesture_detector.dart';
 import 'package:debateseason_frontend_v1/widgets/de_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -18,39 +19,49 @@ class DeBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(24, 0, 24, 16),
-      decoration: BoxDecoration(
-        color: grey80,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
+    return DeGestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(24, 0, 24, 16),
+        decoration: BoxDecoration(
+          color: grey80,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          Center(
-            child: _widgetGrabber(),
-          ),
-          Gaps.v16,
-          Row(
-            children: [
-              Expanded(
-                child: DeText(
-                  titleText,
-                  style: title,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: _widgetGrabber(),
+            ),
+            Gaps.v16,
+            Row(
+              children: [
+                Expanded(
+                  child: DeText(
+                    titleText,
+                    style: title,
+                  ),
                 ),
-              ),
-              SvgPicture.asset(
-                'assets/icons/ic_x_grey50.svg',
-                width: 14,
-                height: 14,
-              ),
-            ],
-          ),
-          Gaps.v16,
-          widget,
-        ],
+                DeGestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Padding(
+                    padding: Dimensions.all10,
+                    child: SvgPicture.asset(
+                      'assets/icons/ic_x_grey50.svg',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Gaps.v16,
+            widget,
+          ],
+        ),
       ),
     );
   }
