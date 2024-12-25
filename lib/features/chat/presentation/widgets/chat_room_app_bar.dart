50f4b1c8455ac46bcb719106838d768754308119
Anzinius <anzinius@gmail.com>
2024-12-25 21:21:24 +0900

    mod #38 토론방 앱 바 패딩 수정

--- lib/features/chat/presentation/widgets/chat_room_app_bar.dart
+++ lib/features/chat/presentation/widgets/chat_room_app_bar.dart
@@ -14,28 +14,41 @@ class ChatRoomAppBar extends StatelessWidget implements PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      titleSpacing: 0,
       backgroundColor: grey80,
       automaticallyImplyLeading: false,
-      title: Row(
+      title: Column(
         children: [
-          Expanded(
-            child: Text(
-              title,
-              style: body14Sb.copyWith(color: grey10),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Gaps.h12,
-          Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.14),
-            child: SizedBox(
-              width: 24.0,
-              height: 24.0,
-              child: SvgPicture.asset(
-                'assets/icons/ic_more_grey10.svg',
+          Gaps.v8,
+          Row(
+            children: [
+              Gaps.h20,
+              Expanded(
+                child: Text(
+                  title,
+                  style: body14Sb.copyWith(color: grey10),
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-            ),
+              Gaps.h12,
+              Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.14),
+                child: SizedBox(
+                  width: 24.0,
+                  height: 24.0,
+                  child: SvgPicture.asset(
+                    'assets/icons/ic_more_grey10.svg',
+                  ),
+                ),
+              ),
+              Gaps.h20,
+            ],
+          ),
+          Gaps.v8,
+          Container(
+            height: 1,
+            color: grey70,
           )
         ],
       ),
