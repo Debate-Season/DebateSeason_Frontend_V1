7159599a1fa7004ec837e6af84c86df41ae05952
Anzinius <anzinius@gmail.com>
2024-12-30 02:48:45 +0900

    feat #38 채팅용 바텀시트 위젯 생성

---
+++ lib/features/chat/presentation/widgets/chat_bottom_sheet.dart
@@ -0,0 +1,59 @@
+import 'package:debateseason_frontend_v1/core/constants/color.dart';
+import 'package:debateseason_frontend_v1/core/constants/dimensions.dart';
+import 'package:debateseason_frontend_v1/core/constants/gaps.dart';
+import 'package:debateseason_frontend_v1/widgets/de_gesture_detector.dart';
+import 'package:flutter/material.dart';
+
+class chatBottomSheet extends StatelessWidget {
+  final Widget widget;
+
+  const chatBottomSheet({
+    super.key,
+    required this.widget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DeGestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(24, 0, 24, 16),
+        decoration: BoxDecoration(
+          color: grey80,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: _widgetGrabber(),
+            ),
+            Gaps.v16,
+            Row(
+              children: [
+
+              ],
+            ),
+            Gaps.v16,
+            widget,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _widgetGrabber() {
+    return Container(
+      width: 40,
+      height: 5,
+      margin: Dimensions.all8,
+      decoration: BoxDecoration(
+        color: grey50,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+}
