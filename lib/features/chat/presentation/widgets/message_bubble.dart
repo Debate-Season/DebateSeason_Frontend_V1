897ebf9aa327c024d031017093a8864d50b5c445
Anzinius <anzinius@gmail.com>
2025-01-06 00:20:55 +0900

    feat #38 메세지 버블에 발신자 정보 출력

--- lib/features/chat/presentation/widgets/message_bubble.dart
+++ lib/features/chat/presentation/widgets/message_bubble.dart
@@ -1,8 +1,11 @@
 import 'package:debateseason_frontend_v1/core/constants/color.dart';
 import 'package:debateseason_frontend_v1/core/constants/dimensions.dart';
+import 'package:debateseason_frontend_v1/core/constants/gaps.dart';
 import 'package:debateseason_frontend_v1/core/constants/text_style.dart';
 import 'package:debateseason_frontend_v1/features/chat/data/models/request/message_request.dart';
+import 'package:debateseason_frontend_v1/widgets/de_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MessageBubble extends StatelessWidget {
   final MessageRequest message;
@@ -22,10 +25,25 @@ class MessageBubble extends StatelessWidget {
       bottomRight: Radius.circular(12.0),
     );
 
-    return Align(
-      alignment: alignment,
-      child: Container(
-        margin: Dimensions.all20,
+    Widget msgHeader() {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _myProfile(),
+          Gaps.h4,
+          _myId(),
+          Gaps.h4,
+          _myCommunity(),
+          Gaps.h4,
+          SvgPicture.asset('assets/icons/ic_dot_grey50.svg'),
+          Gaps.h4,
+          _sendTime(),
+        ],
+      );
+    }
+
+    Widget msgBody() {
+      return Container(
         padding: Dimensions.all12,
         decoration: BoxDecoration(
           color: backgroundColor,
@@ -35,7 +53,46 @@ class MessageBubble extends StatelessWidget {
           message.content,
           style: body14M,
         ),
+      );
+    }
+
+    return Container(
+      margin: Dimensions.all20,
+      child: Column(
+        children: [
+          Align(
+            alignment: alignment,
+            child: msgHeader(),),
+          Gaps.v4,
+          Align(
+            alignment: alignment,
+            child: msgBody(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myProfile() {
+    return Container(
+      width: 20,
+      height: 20,
+      decoration: BoxDecoration(
+        color: Color(0xFFE2B84C),
+        borderRadius: BorderRadius.circular(50),
       )
     );
+  }
+
+  Widget _myId() {
+    return DeText('난중일기', style: cation12SB.copyWith(color: grey10),);
+  }
+
+  Widget _myCommunity() {
+    return DeText('에펨코리아', style: cation12R.copyWith(color: grey30),);
+  }
+
+  Widget _sendTime() {
+    return DeText('20:05', style: cation12R.copyWith(color: grey30),);
   }
 }
