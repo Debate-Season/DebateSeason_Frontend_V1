6688406654b455bf29031d14e3e8e1bae479c0f2
Doreminwoo <kimy5878@naver.com>
2025-01-15 22:26:02 +0900

    feat #50 post/auth-reissue 구현

--- lib/features/auth/data/repository_impls/remote/auth_reissue_repository_impl.dart
+++ lib/features/auth/data/repository_impls/remote/auth_reissue_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:debateseason_frontend_v1/core/services/secure_storage_service.dart';
 import 'package:debateseason_frontend_v1/features/auth/data/data_sources/remote/auth_reissue_data_source.dart';
 import 'package:debateseason_frontend_v1/features/auth/data/mapper/auth_reissue_mapper.dart';
 import 'package:debateseason_frontend_v1/features/auth/domain/entities/auth_reissue_entity.dart';
@@ -9,13 +10,20 @@ class AuthReissueRepositoryImpl implements AuthReissueRepository {
   AuthReissueRepositoryImpl(this.dataSource);
 
   @override
-  Future<AuthReissueEntity> postAuthReissue({
+  Future<void> postAuthReissue({
     required AuthReissueEntity entity,
   }) async {
+    final SecureStorageService storage = SecureStorageService();
+
     final response = await dataSource.postAuthReissue(
       body: AuthReissueMapper.toRequest(entity: entity),
     );
 
-    return AuthReissueMapper.toEntity(res: response.data);
+    final authReissue = AuthReissueMapper.toEntity(res: response.data);
+
+    await Future.wait([
+      storage.setAccessToken(accessToken: authReissue.accessToken),
+      storage.setRefreshToken(refreshToken: authReissue.accessToken),
+    ]);
   }
 }
