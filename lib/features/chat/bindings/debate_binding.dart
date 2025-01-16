d9d47d6eea0c9399cd107d87fdd6d8294acc51a0
Anzinius <anzinius@gmail.com>
2025-01-17 08:29:49 +0900

    feat #53 바인딩 추가

--- lib/features/chat/bindings/debate_binding.dart
+++ lib/features/chat/bindings/debate_binding.dart
@@ -1,3 +1,5 @@
+import 'package:debateseason_frontend_v1/core/network/dio_client.dart';
+import 'package:debateseason_frontend_v1/features/chat/data/data_sources/room_data_source.dart';
 import 'package:debateseason_frontend_v1/features/chat/presentation/view_model/debate_room_view_model.dart';
 import 'package:get/get.dart';
 
@@ -5,5 +7,6 @@ class DebateBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => DebateRoomViewModel());
+    Get.lazyPut<RoomDataSource>(() => RoomDataSource(DioClient().dio));
   }
 }
