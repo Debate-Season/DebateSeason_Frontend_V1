924500e8c56a7fc2526cd3286b92a6b62f55e843
Anzinius <anzinius@gmail.com>
2025-01-17 08:29:16 +0900

    feat #53 채팅방api 뷰모델 사용예시코드 추가

--- lib/features/chat/presentation/view_model/debate_room_view_model.dart
+++ lib/features/chat/presentation/view_model/debate_room_view_model.dart
@@ -1,11 +1,22 @@
+import 'package:debateseason_frontend_v1/features/chat/data/data_sources/room_data_source.dart';
 import 'package:get/get.dart';
 
 class DebateRoomViewModel extends GetxController {
+  late final RoomDataSource _roomDataSource;
+
   RxInt agreeCount = 0.obs;
   RxInt disagreeCount = 0.obs;
 
-  void voteAgree() {
+  @override
+  void onInit() {
+    super.onInit();
+    _roomDataSource = Get.find<RoomDataSource>();
+  }
+
+  void voteAgree() async{
     agreeCount++;
+    final agree = await _roomDataSource.getRoom(chatroomId: 1);
+    print(agree.data.opinion);
   }
 
   void voteDisagree() {
