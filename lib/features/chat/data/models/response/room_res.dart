d866ae27d7b20ea86ae693ce51d90653eb2a0ab9
Anzinius <anzinius@gmail.com>
2025-01-17 08:26:47 +0900

    feat #53 채팅방 데이터모델 정의

---
+++ lib/features/chat/data/models/response/room_res.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'room_res.g.dart';
+
+@JsonSerializable()
+class RoomRes {
+  int chatRoomId;
+  String title;
+  String content;
+  int agree;
+  int disagree;
+  String createdAt;
+  String opinion;
+
+   RoomRes({
+    required this.chatRoomId,
+    required this.title,
+    required this.content,
+    required this.agree,
+    required this.disagree,
+    required this.createdAt,
+    required this.opinion,
+  });
+
+  factory RoomRes.fromJson(Map<String, dynamic> json) =>
+      _$RoomResFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RoomResToJson(this);
+}
