5b9ad062339b6211784528bdc73718df3421d2d5
Doreminwoo <kimy5878@naver.com>
2025-01-20 22:30:12 +0900

    mod #59 프로필 수정시 프로필 화면 업데이트

--- lib/features/profile/presentation/view_models/profile_input_view_model.dart
+++ lib/features/profile/presentation/view_models/profile_input_view_model.dart
@@ -5,6 +5,7 @@ import 'package:debateseason_frontend_v1/features/profile/domain/entities/profil
 import 'package:debateseason_frontend_v1/features/profile/domain/repositories/remote/community_repository.dart';
 import 'package:debateseason_frontend_v1/features/profile/domain/repositories/remote/profile_nickname_check_repository.dart';
 import 'package:debateseason_frontend_v1/features/profile/domain/repositories/remote/profile_repository.dart';
+import 'package:debateseason_frontend_v1/features/profile/presentation/view_models/profile_view_model.dart';
 import 'package:debateseason_frontend_v1/features/profile/profile_constants.dart';
 import 'package:debateseason_frontend_v1/utils/base/ui_state.dart';
 import 'package:debateseason_frontend_v1/utils/de_snack_bar.dart';
@@ -13,6 +14,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfileInputViewModel extends GetxController {
+  late ProfileViewModel _profileViewModel;
   late TextEditingController nicknameController;
   late TextEditingController communityController;
   late TextEditingController communitySearchController;
@@ -79,6 +81,7 @@ class ProfileInputViewModel extends GetxController {
 
     if (receivedData != null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
+        _profileViewModel = Get.find<ProfileViewModel>();
         final previousProfile = Get.arguments as ProfileEntity;
         _profile.value = previousProfile;
         _previousNickname.value = _profile.value.nickname;
@@ -142,10 +145,13 @@ class ProfileInputViewModel extends GetxController {
         entity: _profile.value,
       );
 
-  Future<UiState<String>> patchProfile() async =>
-      await _profileRepository.patchProfile(
-        entity: _profile.value,
-      );
+  Future<UiState<String>> patchProfile() async {
+    _profileViewModel.updateProfile(profile: _profile.value);
+
+    return await _profileRepository.patchProfile(
+      entity: _profile.value,
+    );
+  }
 
   void onChangedNickname({required String nickname}) {
     if (nickname == _previousNickname.value) {
