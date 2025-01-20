9b41acfac151a2e593dc7471cfb0d274b4da6e57
Anzinius <anzinius@gmail.com>
2025-01-21 02:34:31 +0900

    feat #52 issue api 뷰모델 작성

--- lib/features/issue/presentation/view_model/issue_room_view_model.dart
+++ lib/features/issue/presentation/view_model/issue_room_view_model.dart
@@ -1,9 +1,30 @@
+import 'package:debateseason_frontend_v1/features/issue/data/datasources/remote/issue_data_source.dart';
+import 'package:debateseason_frontend_v1/features/issue/data/models/remote/response/issue_res.dart';
+import 'package:debateseason_frontend_v1/utils/logger.dart';
 import 'package:get/get.dart';
 
 class IssueRoomViewModel extends GetxController {
+  late final IssueDataSource _issueDataSource;
+  final Rx<IssueRes?> _issueData = Rx<IssueRes?>(null);
+  IssueRes? get issueData => _issueData.value;
 
   @override
   void onInit() {
     super.onInit();
+    _issueDataSource = Get.find<IssueDataSource>();
+
+    final int issueId = (Get.arguments) ?? 1;
+    log.d('issueId : $issueId');
+    fetchIssueData(issueId);
+  }
+
+  Future<void> fetchIssueData(int issueId) async {
+    try {
+      final response = await _issueDataSource.getIssue(issueId: issueId);
+      _issueData.value = response.data;
+      log.d(_issueData.value);
+    } catch (e) {
+      log.d(e);
+    }
   }
 }
