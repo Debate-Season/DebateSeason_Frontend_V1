c899d3e2fdcb2a5337fabb90deaaae421c818f7f
Doreminwoo <kimy5878@naver.com>
2025-01-23 22:18:56 +0900

    mod #53 StompService 수정
    스트림과 싱글톤을 이용하여 구독할 수 있도록 수정

--- lib/core/services/web_socket/stomp_service.dart
+++ lib/core/services/web_socket/stomp_service.dart
@@ -1,21 +1,33 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:debateseason_frontend_v1/features/chat/domain/entities/chat_message_entity.dart';
+import 'package:debateseason_frontend_v1/utils/logger.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:stomp_dart_client/stomp_dart_client.dart';
 
-import 'package:debateseason_frontend_v1/utils/logger.dart';
-
 class StompService {
-  late StompClient stompClient;
+  static final StompService _instance = StompService._internal();
 
-  void connect(String url, Function(StompFrame) onConnect, Function(dynamic) onError){
+  factory StompService() => _instance;
+
+  StompService._internal();
+
+  late StompClient stompClient;
+  final _chatStream = StreamController<ChatMessageEntity>.broadcast();
+
+  Stream<ChatMessageEntity> get chatStream => _chatStream.stream;
+
+  void connectStomp({required int chatRoomId}) {
     stompClient = StompClient(
       config: StompConfig(
-        url: url,
+        url: dotenv.get("WEB_SOCKET_BASE_URL"),
         onConnect: (frame) {
-          log.d("STOMP 연결 성공: ${frame.headers}");
-          onConnect(frame);
+          log.d("STOMP 연결 성공");
+          _subscribeStomp(chatRoomId: chatRoomId);
         },
         onWebSocketError: (error) {
           log.d("WebSocket 에러: $error");
-          onError(error);
         },
         onStompError: (frame) {
           log.d("STOMP 프로토콜 에러: ${frame.body}");
@@ -32,33 +44,47 @@ class StompService {
       ),
     );
     stompClient.activate();
-    log.d("STOMP 클라이언트 활성화...");
   }
 
-  void subscribe(String destination, Function(String) onMessage) {
-    log.d('* 구독 시도: $destination');
+  void _subscribeStomp({required int chatRoomId}) {
     try {
       stompClient.subscribe(
-        destination: destination,
-        //callback: (frame) => onMessage(frame.body!),
+        destination: '/topic/room$chatRoomId',
         callback: (frame) {
-          log.d('* 구독 성공: $destination, 받은 메시지: ${frame.body}');
-          onMessage(frame.body ?? "");
+          log.d('[Stomp Receive] : $chatRoomId\n${frame.body}');
+          if (frame.body != null) {
+            final chatMessage = ChatMessageEntity.fromJson(
+              jsonDecode(frame.body!),
+            );
+            _chatStream.add(chatMessage);
+          }
         },
       );
-    } catch (e) {
-      log.d('* 구독 실패: $e');
+    } catch (e, stack) {
+      log.d('$e \n $stack');
     }
   }
 
-  void sendMessage(String destination, String message) {
-    stompClient.send(
-      destination: destination,
-      body: message,
-    );
+  void sendStomp({
+    required int chatRoomId,
+    required ChatMessageEntity chatMessage,
+  }) {
+    try {
+      log.d('[Stomp Send] : $chatRoomId\n$chatMessage');
+      stompClient.send(
+        destination: '/topic/room$chatRoomId',
+        body: jsonEncode(chatMessage.toJson()),
+      );
+    } catch (e, stack) {
+      log.d('$e \n $stack');
+    }
   }
 
   void disconnect() {
-    stompClient.deactivate();
+    try {
+      stompClient.deactivate();
+    } catch (e, stack) {
+      log.d('$e \n $stack');
+    }
   }
 }
