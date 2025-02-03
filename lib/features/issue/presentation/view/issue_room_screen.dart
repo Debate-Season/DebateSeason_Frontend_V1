24ba62577660515ecc185ef05812332d588cfa61
Doreminwoo <kimy5878@naver.com>
2025-02-03 22:59:20 +0900

    mod #53 이슈룸 에러 수정 및 issuetitle 변수 제거

--- lib/features/issue/presentation/view/issue_room_screen.dart
+++ lib/features/issue/presentation/view/issue_room_screen.dart
@@ -4,7 +4,7 @@ import 'package:debateseason_frontend_v1/core/constants/gaps.dart';
 import 'package:debateseason_frontend_v1/core/constants/text_style.dart';
 import 'package:debateseason_frontend_v1/core/routers/get_router_name.dart';
 import 'package:debateseason_frontend_v1/features/issue/data/models/remote/response/chat_room_res.dart';
-import 'package:debateseason_frontend_v1/features/issue/presentation//view_model/issue_room_view_model.dart';
+import 'package:debateseason_frontend_v1/features/issue/presentation/view_model/issue_room_view_model.dart';
 import 'package:debateseason_frontend_v1/features/issue/presentation/widgets/issue_app_bar.dart';
 import 'package:debateseason_frontend_v1/features/issue/presentation/widgets/issue_card.dart';
 import 'package:debateseason_frontend_v1/utils/logger.dart';
@@ -19,13 +19,11 @@ import 'package:get/get.dart';
 class IssueRoomScreen extends GetView<IssueRoomViewModel> {
   const IssueRoomScreen({super.key});
 
-  String get issuetitle => controller.issuetitle.value;
-
   @override
   Widget build(BuildContext context) {
     return DeScaffold(
       appBar: IssueAppBar(
-        title: Obx(() => Text(issuetitle, style: title)),
+        title: Obx(() => Text(controller.issuetitle.value, style: title)),
         isCenter: false,
       ),
       body: _body(),
