1dbb34c2320d2cadf7364a03fa58fd5a61c285bb
Doreminwoo <kimy5878@naver.com>
2025-02-06 00:28:30 +0900

    mod #71 AuthBinding

--- lib/features/auth/bindings/auth_binding.dart
+++ lib/features/auth/bindings/auth_binding.dart
@@ -6,6 +6,9 @@ import 'package:debateseason_frontend_v1/features/auth/data/repository_impls/rem
 import 'package:debateseason_frontend_v1/features/auth/domain/repositories/remote/auth_reissue_repository.dart';
 import 'package:debateseason_frontend_v1/features/auth/domain/repositories/remote/users_login_repository.dart';
 import 'package:debateseason_frontend_v1/features/auth/presentation/view_models/auth_view_model.dart';
+import 'package:debateseason_frontend_v1/features/profile/data/data_sources/remote/profile_data_source.dart';
+import 'package:debateseason_frontend_v1/features/profile/data/repository_impls/remote/profile_repository_impl.dart';
+import 'package:debateseason_frontend_v1/features/profile/domain/repositories/remote/profile_repository.dart';
 import 'package:get/get.dart';
 
 class AuthBinding extends Bindings {
@@ -23,6 +26,14 @@ class AuthBinding extends Bindings {
     Get.lazyPut<AuthReissueRepository>(
       () => AuthReissueRepositoryImpl(Get.find<AuthReissueDataSource>()),
     );
+
+    Get.lazyPut<ProfileDataSource>(
+      () => ProfileDataSource(DioClient().dio),
+    );
+    Get.lazyPut<ProfileRepository>(
+      () => ProfileRepositoryImpl(Get.find<ProfileDataSource>()),
+    );
+
     Get.lazyPut(() => AuthViewModel());
   }
 }
