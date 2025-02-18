4e410b5617b7b3a43bd84649a90f2541bc4f27b6
Doreminwoo <kimy5878@naver.com>
2025-02-19 01:04:45 +0900

    chore #93 lint 오류 수정

--- lib/core/constants/icons.dart
+++ lib/core/constants/icons.dart
@@ -1,10 +1,10 @@
 //colorable
-const ic_add_grey30 = 'assets/icons/ic_send_grey30.svg';
-const ic_back_grey50 = 'assets/icons/ic_back_grey50.svg';
-const ic_more_grey10 = 'assets/icons/ic_more_grey10.svg';
-const ic_send_grey10 = 'assets/icons/ic_send_grey10.svg';
-const ic_x_grey50 = 'assets/icons/ic_x_grey50.svg';
+const icAddGrey30 = 'assets/icons/ic_send_grey30.svg';
+const icBackGrey50 = 'assets/icons/ic_back_grey50.svg';
+const icMoreGrey10 = 'assets/icons/ic_more_grey10.svg';
+const icSendGrey10 = 'assets/icons/ic_send_grey10.svg';
+const icXGrey50 = 'assets/icons/ic_x_grey50.svg';
 
 //none colorable
-const ic_kakao_login = 'assets/icons/ic_kakao_login.svg';
-const ic_apple_login = 'assets/icons/ic_apple_login.svg';
+const icKakaoLogin = 'assets/icons/ic_kakao_login.svg';
+const icAppleLogin = 'assets/icons/ic_apple_login.svg';
