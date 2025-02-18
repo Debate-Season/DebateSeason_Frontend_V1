076fb78a7ef23b9e48cd98a6d7b87d9765b03829
Doreminwoo <kimy5878@naver.com>
2025-02-19 01:07:40 +0900

    chore #93 lint

--- lib/features/chat/presentation/widgets/change_popup.dart
+++ lib/features/chat/presentation/widgets/change_popup.dart
@@ -1,27 +1,26 @@
+import 'package:debateseason_frontend_v1/core/constants/color.dart';
 import 'package:debateseason_frontend_v1/core/constants/dimensions.dart';
 import 'package:debateseason_frontend_v1/core/constants/gaps.dart';
+import 'package:debateseason_frontend_v1/core/constants/text_style.dart';
+import 'package:debateseason_frontend_v1/utils/logger.dart';
 import 'package:debateseason_frontend_v1/widgets/de_text.dart';
 import 'package:flutter/material.dart';
 
-import 'package:debateseason_frontend_v1/core/constants/color.dart';
-import 'package:debateseason_frontend_v1/core/constants/text_style.dart';
-import 'package:debateseason_frontend_v1/utils/logger.dart';
-
 class ChangePopup extends StatelessWidget {
-  final String popuptitle;
+  final String popupTitle;
   final String description;
   final String btn1;
   final String btn2;
   final VoidCallback onConfirm;
 
   const ChangePopup({
-    Key? key,
-    required this.popuptitle,
+    super.key,
+    required this.popupTitle,
     required this.description,
     required this.btn1,
     required this.btn2,
     required this.onConfirm,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +32,7 @@ class ChangePopup extends StatelessWidget {
         padding: Dimensions.padding20x16,
         child: Column(
           children: [
-            DeText(popuptitle, style: title.copyWith(color: grey10)),
+            DeText(popupTitle, style: title.copyWith(color: grey10)),
             Gaps.v8,
             DeText(description, style: body14M.copyWith(color: grey30)),
             Gaps.v20,
