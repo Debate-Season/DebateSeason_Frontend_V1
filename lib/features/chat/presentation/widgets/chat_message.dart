95ef2fba842073ff055b439737449e6b8e253c24
yangseungmin <ysm287@gmail.com>
2025-02-24 22:32:19 +0900

    feat #88 ChatMessage 위젯 ChatMessageModel 적용

--- lib/features/chat/presentation/widgets/chat_message.dart
+++ lib/features/chat/presentation/widgets/chat_message.dart
@@ -1,33 +1,32 @@
 import 'package:debateseason_frontend_v1/core/constants/de_colors.dart';
 import 'package:debateseason_frontend_v1/core/constants/de_dimensions.dart';
+import 'package:debateseason_frontend_v1/core/constants/de_fonts.dart';
 import 'package:debateseason_frontend_v1/core/constants/de_gaps.dart';
 import 'package:debateseason_frontend_v1/core/constants/de_icons.dart';
-import 'package:debateseason_frontend_v1/core/constants/de_fonts.dart';
 import 'package:debateseason_frontend_v1/features/chat/chat_constants.dart';
-import 'package:debateseason_frontend_v1/features/chat/domain/entities/chat_message_entity.dart';
-import 'package:debateseason_frontend_v1/features/chat/presentation/types/chat_message_type.dart';
+import 'package:debateseason_frontend_v1/features/chat/data/models/response/chat_message_model.dart';
+import 'package:debateseason_frontend_v1/utils/date_format_util.dart';
 import 'package:debateseason_frontend_v1/widgets/de_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class ChatMessage extends StatelessWidget {
-  final ChatMessageEntity message;
+  final ChatMessageModel chatMessageModel;
+  final bool withDateLine;
 
-  const ChatMessage({super.key, required this.message});
+  const ChatMessage(this.chatMessageModel,
+      {super.key, required this.withDateLine});
 
   @override
   Widget build(BuildContext context) {
-    if (message.messageType == ChatMessageType.date.value) {
-      return _dateline();
-    }
-
-    switch (message.opinionType) {
+    switch (chatMessageModel.opinionType) {
       case ChatConstants.chatAgree:
         return Align(
           alignment: Alignment.centerLeft,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              if (withDateLine) Align(child: _dateline()),
               _chatMessageHeader(),
               DeGaps.v4,
               _chatMessageBody(
@@ -94,19 +93,20 @@ class ChatMessage extends StatelessWidget {
         ),
         DeGaps.h4,
         DeText(
-          message.sender,
+          chatMessageModel.sender,
           style: DeFonts.caption12SB.copyWith(color: DeColors.grey10),
         ),
         DeGaps.h4,
         DeText(
-          message.userCommunity,
+          chatMessageModel.userCommunity,
           style: DeFonts.caption12R.copyWith(color: DeColors.grey30),
         ),
         DeGaps.h4,
         SvgPicture.asset(DeIcons.icDotGrey50),
         DeGaps.h4,
         DeText(
-          _formatMessageTime(time: message.timeStamp),
+          // chatMessageModel.timeStamp,
+          _formatMessageTime(time: DateTime.parse(chatMessageModel.timeStamp)),
           style: DeFonts.caption12R.copyWith(color: DeColors.grey30),
         ),
       ],
@@ -124,28 +124,36 @@ class ChatMessage extends StatelessWidget {
         borderRadius: borderRadius,
       ),
       child: DeText(
-        message.content,
+        chatMessageModel.content,
         style: DeFonts.body14M,
       ),
     );
   }
 
   Widget _dateline() {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      children: [
-        Container(
-          padding: DeDimensions.padding12x4,
-          decoration: BoxDecoration(
-            color: DeColors.grey90,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: DeText(
-            message.content,
-            style: DeFonts.caption12R.copyWith(color: DeColors.grey50),
-          ),
+    final date = DateTime.parse(chatMessageModel.timeStamp);
+
+    String dateString;
+    if (DateFormatUtil.isToday(date)) {
+      dateString = "오늘";
+    } else if (DateFormatUtil.isYesterday(date)) {
+      dateString = "오늘";
+    } else {
+      dateString = "${date.year}.${date.month}.${date.day}";
+    }
+
+    return Align(
+      child: Container(
+        padding: DeDimensions.padding12x4,
+        decoration: BoxDecoration(
+          color: DeColors.grey90,
+          borderRadius: BorderRadius.circular(20.0),
         ),
-      ],
+        child: DeText(
+          dateString,
+          style: DeFonts.caption12R.copyWith(color: DeColors.grey50),
+        ),
+      ),
     );
   }
 
