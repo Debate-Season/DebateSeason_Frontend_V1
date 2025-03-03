de24305b84eab513b12a3765f553080f4a2ca613
Anzinius <anzinius@gmail.com>
2025-03-04 00:55:13 +0900

    add #80 응답데이터 모델에 엔티티 변환 Mapper 추가

--- lib/features/issue/data/models/remote/response/issue_res.dart
+++ lib/features/issue/data/models/remote/response/issue_res.dart
@@ -1,4 +1,5 @@
 import 'package:debateseason_frontend_v1/features/issue/data/models/remote/response/chat_room_res.dart';
+import 'package:debateseason_frontend_v1/features/issue/domain/entities/issue_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'issue_res.g.dart';
@@ -13,11 +14,16 @@ class IssueRes {
     required this.title,
     required this.map,
     required this.chatRoomMap,
-});
-
+  });
 
   factory IssueRes.fromJson(Map<String, dynamic> json) =>
       _$IssueResFromJson(json);
 
   Map<String, dynamic> toJson() => _$IssueResToJson(this);
+
+  IssueEntity toEntity() => IssueEntity(
+        title: title,
+        map: map,
+        chatRoomMap: chatRoomMap,
+      );
 }
