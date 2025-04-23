14c7e0d966ece4926a4a061d77cff85775dec01e
Anzinius <67458912+Anzinius@users.noreply.github.com>
2025-04-23 21:40:21 +0900

    add #114 안드로이드 카카오로그인 관련 설정 추가

--- lib/features/home/presentation/widgets/issue_card.dart
+++ lib/features/home/presentation/widgets/issue_card.dart
@@ -1,9 +1,7 @@
 import 'package:debateseason_frontend_v1/core/constants/de_colors.dart';
 import 'package:debateseason_frontend_v1/core/constants/de_gaps.dart';
 import 'package:debateseason_frontend_v1/core/constants/de_fonts.dart';
-import 'package:debateseason_frontend_v1/features/issue/domain/entities/chat_room_entity.dart';
 import 'package:debateseason_frontend_v1/features/issue/issue_constants.dart';
-import 'package:debateseason_frontend_v1/features/issue/presentation/view_models/issue_ratio_view_model.dart';
 import 'package:debateseason_frontend_v1/widgets/de_text.dart';
 import 'package:flutter/material.dart';
 
