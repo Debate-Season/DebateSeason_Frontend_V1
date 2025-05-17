642194bffd76ef735f5c71b0cdf9aa415371b620
Anzinius <67458912+Anzinius@users.noreply.github.com>
2025-05-18 00:26:27 +0900

    add #138 home screen (더미) 추가

---
+++ lib/features/home/presentation/views/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:debateseason_frontend_v1/core/constants/de_fonts.dart';
+import 'package:debateseason_frontend_v1/features/home/presentation/view_models/home_view_model.dart';
+import 'package:debateseason_frontend_v1/widgets/de_scaffold.dart';
+import 'package:debateseason_frontend_v1/widgets/de_text.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends GetView<HomeViewModel> {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DeScaffold(
+      body: Obx(
+            () => Text(
+          controller.recommendData.when(
+            loading: () => '로딩중',
+            success: (issueData) => 'recommendData.title',
+            failure: (error) => error,
+          ),
+          style: DeFonts.header18Sb,
+        ),
+      ),
+    );
+  }
+}
