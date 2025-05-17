bf084bada93beb4cff3bc78d883005d17336608b
Anzinius <67458912+Anzinius@users.noreply.github.com>
2025-05-18 00:10:34 +0900

    add #138 entity 추가

---
+++ lib/features/home/domain/entities/best_issue_room_entity.dart
@@ -0,0 +1,13 @@
+class BestIssueRoomEntity {
+  final int issueId;
+  final String title;
+  final int countChatRoom;
+  final int bookMarks;
+
+  BestIssueRoomEntity({
+    required this.issueId,
+    required this.title,
+    required this.countChatRoom,
+    required this.bookMarks,
+  });
+}
---
+++ lib/features/home/domain/entities/breaking_news_entity.dart
@@ -0,0 +1,9 @@
+class BreakingNewsEntity {
+  final String title;
+  final String url;
+
+  BreakingNewsEntity({
+    required this.title,
+    required this.url,
+  });
+}
