b4133a3c4511c6c4f267af3547999d62d50b0a0e
Doreminwoo <kimy5878@naver.com>
2025-06-03 18:47:16 +0900

    feat : 홈 탭바 Amplitude Event

--- lib/features/home/presentation/views/home_screen_content.dart
+++ lib/features/home/presentation/views/home_screen_content.dart
@@ -1,7 +1,10 @@
 import 'package:debateseason_frontend_v1/core/constants/de_dimensions.dart';
 import 'package:debateseason_frontend_v1/features/home/presentation/widgets/home_tab_bar.dart';
 import 'package:debateseason_frontend_v1/features/home/presentation/widgets/home_tab_view.dart';
+import 'package:debateseason_frontend_v1/utils/amplitude_util.dart';
+import 'package:debateseason_frontend_v1/utils/logger.dart';
 import 'package:flutter/material.dart';
+
 import 'home_media_page.dart';
 import 'home_recommend_page.dart';
 
@@ -21,7 +24,19 @@ class _HomeScreenContentState extends State<HomeScreenContent>
   @override
   void initState() {
     super.initState();
+    AmplitudeUtil.trackEvent(eventName: 'Home').then((_) {
+      log.d('home');
+    });
     _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) return;
+
+      final eventNames = ['홈탭미디어', '홈탭이슈·토론'];
+      AmplitudeUtil.trackEvent(eventName: eventNames[_tabController.index])
+          .then((_) {
+        log.d(eventNames[_tabController.index]);
+      });
+    });
   }
 
   @override
@@ -63,7 +78,7 @@ class _HomeScreenContentState extends State<HomeScreenContent>
     );
   }
 
- /* Widget _headlineNews() {
+/* Widget _headlineNews() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 12, 20, 4),
       child: Column(
