e796aca2fc249eb83325da48a704e8320ed1c727
Anzinius <67458912+Anzinius@users.noreply.github.com>
2025-06-03 19:20:36 +0900

    add #144 라이브 관련 뷰모델 정의

--- lib/features/home/bindings/media_binding.dart
+++ lib/features/home/bindings/media_binding.dart
@@ -1,7 +1,11 @@
 import 'package:debateseason_frontend_v1/core/network/dio_client.dart';
+import 'package:debateseason_frontend_v1/features/home/data/datasources/live_all_data_source.dart';
 import 'package:debateseason_frontend_v1/features/home/data/datasources/media_data_source.dart';
+import 'package:debateseason_frontend_v1/features/home/data/repository_impls/live_all_repository_impl.dart';
 import 'package:debateseason_frontend_v1/features/home/data/repository_impls/media_repository_impl.dart';
+import 'package:debateseason_frontend_v1/features/home/domain/repositories/live_all_repository.dart';
 import 'package:debateseason_frontend_v1/features/home/domain/repositories/media_repository.dart';
+import 'package:debateseason_frontend_v1/features/home/presentation/view_models/live_view_model.dart';
 import 'package:debateseason_frontend_v1/features/home/presentation/view_models/media_view_model.dart';
 import 'package:get/get.dart';
 
@@ -11,5 +15,8 @@ class MediaBinding extends Bindings {
     Get.lazyPut(() => MediaViewModel());
     Get.lazyPut<MediaDataSource>(() => MediaDataSource(DioClient().dio));
     Get.lazyPut<MediaRepository>(() => MediaRepositoryImpl(Get.find<MediaDataSource>()));
+    Get.lazyPut(()=> LiveViewModel());
+    Get.lazyPut<LiveAllDataSource>(() => LiveAllDataSource(DioClient().dio));
+    Get.lazyPut<LiveAllRepository>(() => LiveAllRepositoryImpl(Get.find<LiveAllDataSource>()));
   }
 }
--- lib/features/home/presentation/view_models/media_view_model.dart
+++ lib/features/home/presentation/view_models/media_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:debateseason_frontend_v1/features/home/data/models/media/media_item_res.dart';
 import 'package:debateseason_frontend_v1/features/home/domain/entities/media_entity.dart';
 import 'package:debateseason_frontend_v1/features/home/domain/repositories/media_repository.dart';
 import 'package:debateseason_frontend_v1/utils/base/ui_state.dart';
@@ -10,6 +11,9 @@ class MediaViewModel extends GetxController {
   final _mediaData = Rx<UiState<MediaEntity>>(const UiState.loading());
 
   UiState<MediaEntity> get mediaData => _mediaData.value;
+
+  String? lastFetchedTime;
+  final RxBool isAppending = false.obs;
 
   final showPip = false.obs;
   late YoutubePlayerController youtubePlayerController;
@@ -52,4 +56,48 @@ class MediaViewModel extends GetxController {
       log.d(s);
     }
   }
+
+  // Future<void> appendMediaData({String? type}) async {
+  //   if (isAppending.value || lastFetchedTime == null) return;
+  //
+  //   isAppending.value = true;
+  //
+  //   try {
+  //     final response = await _mediaRepository.getMedia(
+  //       type: type,
+  //       time: lastFetchedTime,
+  //     );
+  //
+  //     // 🟡 response는 UiState<MediaEntity>니까 when으로 분기
+  //     response.when(
+  //       loading: () {
+  //         // 무시 (이미 로딩 중)
+  //       },
+  //       failure: (message) {
+  //         log.e("📛 append 실패: $message");
+  //       },
+  //       success: (newData) {
+  //         final newItems = newData.items;
+  //
+  //         // 현재 상태도 success인 경우에만 병합
+  //         _mediaData.value = _mediaData.value.maybeWhen(
+  //           success: (currentData) {
+  //             final mergedItems = [...currentData.items, ...newItems];
+  //             lastFetchedTime = newItems.isNotEmpty
+  //                 ? newItems.last.createdAt.toIso8601String()
+  //                 : lastFetchedTime;
+  //
+  //             return UiState.success(MediaEntity(items: mergedItems));
+  //           },
+  //           orElse: () => _mediaData.value, // 현재 상태 유지
+  //         );
+  //       },
+  //     );
+  //   } catch (e, s) {
+  //     log.e("🔥 append 예외 발생: $e");
+  //     log.e(s);
+  //   } finally {
+  //     isAppending.value = false;
+  //   }
+  // }
 }
